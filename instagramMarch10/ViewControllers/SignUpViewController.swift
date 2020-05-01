275dbbd621e7c74a20dcac7e0ade85df9eb8b40c
Margiett Gil <mxg627@icloud.com>
2020-05-01 15:02:52 -0400

    finished SignupViewController

--- instagramMarch10/ViewControllers/SignUpViewController.swift
+++ instagramMarch10/ViewControllers/SignUpViewController.swift
@@ -15,22 +15,38 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLayoutSubviews() {
+        signUpButton.layer.cornerRadius = 8
+    }
+    private var authSession = AuthenticationSession()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        guard let email = emailTextField.text, !email.isEmpty,
+            let password = passwordTextField.text, !password.isEmpty
+            else {
+                self.showAlert(title: "Missing feilds", message: "Please provide an email and password")
+                return
+        }
+        authSession.createNewUser(email: email, password: password) { (result) in
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showAlert(title: "Error Signing up", message: "\(error.localizedDescription)")
+                }
+            case .success:
+                DispatchQueue.main.async {
+                    UIViewController.showViewController(storyBoardName: "MainView", viewControllerId: "MainTabBarController")
+                }
+            }
+        }
     }
-    */
+
+    
 
 }
