69018661632ec7ddf91cb1550b1d0e2dbb7272eb
Margiett Gil <mxg627@icloud.com>
2020-05-05 15:32:38 -0400

    finished the EditProfileViewController still have one error in the shareViewController

--- instagramMarch10/ViewControllers/EditProfileViewController.swift
+++ instagramMarch10/ViewControllers/EditProfileViewController.swift
@@ -7,24 +7,120 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Kingfisher
 
 class EditProfileViewController: UIViewController {
-
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    
+    
+    private let storageService = StorageService()
+    
+    private lazy var imagePickerController: UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        return imagePicker
+    }()
+    
+    private var selectedImage: UIImage? {
+        didSet {
+            profilePicture.image = selectedImage
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        profilePicture.layer.cornerRadius = profilePicture.frame.width/2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadUserInfo(){
+        guard let user = Auth.auth().currentUser else {
+            return
+        }
+        if let photoURL = user.photoURL {
+            profilePicture.kf.setImage(with: photoURL)
+        }
+        selectedImage = profilePicture.image
+        if let email = user.displayName {
+            username.text = email
+        }
     }
-    */
+    
+    @IBAction func changePhotoButtonPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Change Profile Photo", message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: "Take Photo", style: .default) { alertAction in
+            self.imagePickerController.sourceType = .camera
+            self.present(self.imagePickerController, animated: true)
+        }
+        let photoLibraryAction = UIAlertAction(title: "Choose from Library", style: .default) { alertController in
+            self.imagePickerController.sourceType = .photoLibrary
+            self.present(self.imagePickerController, animated: true)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alertController.addAction(cameraAction)
+        }
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cancelAction)
+        alertController.view.tintColor = #colorLiteral(red: 0.83209306, green: 0.5366696119, blue: 0.8646921515, alpha: 1)
+        present(alertController, animated: true)
+    }
+    
+    @IBAction func doneEditing(_ sender: Any) {
+        guard let selectedImage = selectedImage else {
+            return
+        }
+        guard let user = Auth.auth().currentUser else { return }
+        
+        let resizedImage = UIImage.resizeImage(originalImage: selectedImage, rect: profilePicture.bounds)
+        
+        storageService.uploadPhoto(userId: user.uid, image: resizedImage) { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                self?.showAlert(title: "Error uploading photo", message: "\(error.localizedDescription)")
+            case .success(let url):
+                let request = Auth.auth().currentUser?.createProfileChangeRequest()
+                
+                if let userName = self?.username.text, !userName.isEmpty {
+                    request?.displayName = userName
+                }
+                request?.photoURL = url
+                
+                request?.commitChanges(completion: { (error) in
+                    if let error = error {
+                        DispatchQueue.main.async {
+                            self?.showAlert(title: "Error updating profile", message: "Error changing an alert \(error.localizedDescription)")
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            self?.showAlert(title: "Profile change", message: "Your profile was successfully created ")
+                        }
+                    }
+                })
+            }
+        }
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+   
+}
 
+extension EditProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+       selectedImage = image
+        dismiss(animated: true)
+    }
 }
